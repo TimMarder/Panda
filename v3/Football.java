916ff594ab753b267a18ad734a2efac78e9df9d7
TimMarder <tmarder@stuy.edu>
2018-01-16 21:08:09 -0500

    Added v3; Created Game.java; edited woo.java, updated OffensivePlayers.java; updated DefensivePlayers.java, updated Team.java, updated individual position java files

---
+++ v3/Football.java
@@ -0,0 +1,241 @@
+//~~~~~~~~IMPORTS~~~~~~~~
+import cs1.Keyboard;
+
+public class Team {
+    
+    //~~~~~~~~INSTANCE VARIABLES~~~~~~~~
+    public String userName;  //the name of the user but will display as "Coach <name>".
+    public String teamName;  //the team that the user chooses to coach.
+    public int teamRating;
+    public int week;
+    public Quarterback myQB;
+    public Receiver myWR1, myWR2, myWR3;
+    public Runningback myRB;
+    public Linebacker myLB1;
+    public Linebacker myLB2;
+    public Cornerback myCB1;
+    public Cornerback myCB2;
+    public Safety mySS;
+
+    //public String[][] menuBoard = new String[2][2]; //the coach’s dashboard for managing the team.
+
+    //~~~~~~~~CONSTRUCTOR~~~~~~~~
+    public Football() {
+	//menuBoard[0][0] = "NAME: ";
+	//menuBoard[1][0] = "TEAM: ";
+    }
+    //~~~~~~~~METHODS~~~~~~~~
+    public void setUserName( String name ) {
+	userName = name;
+	//menuBoard[0][1] = userName;
+    }
+    
+    public void setupUser() {
+	System.out.println("\nHello Coach, What is your name? (Input Name)\n");
+	String name = Keyboard.readString();
+	System.out.println("\nCoach " + name + ", Is that right?");
+	System.out.println("\nType 'Yes' to confirm, 'No' to change name.\n");
+	String confirm = Keyboard.readString();
+	if( confirm.equalsIgnoreCase("Yes") ) {
+	    setUserName(name);
+	    System.out.println("\nWelcome Coach " + name + "!");
+	    return;
+	}
+	if( confirm.equalsIgnoreCase("No") ) {
+	    setupUser();
+	}
+	else {
+	    System.out.println("\nI couldn't quite get that.");
+	    setupUser();
+	}
+    }
+
+    public void setTeamName( String name ) {
+	teamName = name;
+	//menuBoard[1][1] = teamName;
+    }
+
+    public void setupTeam() {
+	System.out.println("\nCoach " + userName + ", What team are you coaching this season? (Giants Eagles Browns Steelers Falcons)\n");
+	String name = Keyboard.readString();
+	if( name.equalsIgnoreCase("Giants") ) {
+	    System.out.println("\nYou chose the Giants. Type 'Yes' to confirm, 'No' to change team.\n");
+	    String confirm = Keyboard.readString();
+	    if( confirm.equalsIgnoreCase("Yes") ) {
+		setTeamName(name.toUpperCase());
+		System.out.println("\nWelcome to the " + teamName + " Coach " + userName + "!");
+		return;
+	    }
+	    if( confirm.equalsIgnoreCase("No") ) {
+		setupTeam();
+	    }
+	    else {
+		System.out.println("\nI couldn't quite get that.");
+		setupTeam();
+	    }
+	    return;
+	}
+	if( name.equalsIgnoreCase("Eagles") ) {
+	    System.out.println("\nYou chose the Eagles. Type 'Yes' to confirm, 'No' to change team.\n");
+	    String confirm = Keyboard.readString();
+	    if( confirm.equalsIgnoreCase("Yes") ) {
+		setTeamName(name);
+		System.out.println("\nWelcome to the " + teamName + " Coach " + userName + "!");
+		return;
+	    }
+	    if( confirm.equalsIgnoreCase("No") ) {
+		setupTeam();
+	    }
+	    else {
+		System.out.println("\nI couldn't quite get that.");
+		setupTeam();
+	    }
+	    return;
+	}
+	if( name.equalsIgnoreCase("Browns") ) {
+	    System.out.println("\nYou chose the Browns. Type 'Yes' to confirm, 'No' to change team.\n");
+	    String confirm = Keyboard.readString();
+	    if( confirm.equalsIgnoreCase("Yes") ) {
+		setTeamName(name);
+		System.out.println("\nWelcome to the " + teamName + " Coach " + userName + "!");
+		return;
+	    }
+	    if( confirm.equalsIgnoreCase("No") ) {
+		setupTeam();
+	    }
+	    else {
+		System.out.println("\nI couldn't quite get that.");
+		setupTeam();
+	    }
+	    return;
+	}
+	if( name.equalsIgnoreCase("Steelers") ) {
+	    System.out.println("\nYou chose the Steelers. Type 'Yes' to confirm, 'No' to change team.\n");
+	    String confirm = Keyboard.readString();
+	    if( confirm.equalsIgnoreCase("Yes") ) {
+		setTeamName(name);
+		System.out.println("\nWelcome to the " + teamName + " Coach " + userName + "!");
+		return;
+	    }
+	    if( confirm.equalsIgnoreCase("No") ) {
+		setupTeam();
+	    }
+	    else {
+		System.out.println("\nI couldn't quite get that.");
+		setupTeam();
+	    }
+	    return;
+	}
+	if( name.equalsIgnoreCase("Falcons") ) {
+	    System.out.println("\nYou chose the Falcons. Type 'Yes' to confirm, 'No' to change team.\n");
+	    String confirm = Keyboard.readString();
+	    if( confirm.equalsIgnoreCase("Yes") ) {
+		setTeamName(name);
+		System.out.println("\nWelcome to the " + teamName + " Coach " + userName + "!");
+		return;
+	    }
+	    if( confirm.equalsIgnoreCase("No") ) {
+		setupTeam();
+	    }
+	    else {
+		System.out.println("\nI couldn't quite get that.");
+		setupTeam();
+	    }
+	    return;
+	}
+	else {
+	    System.out.println("\nI couln't quite get that.");
+	    setupTeam();
+	}
+    }
+
+    public void setWeek(int i) {
+	week = i;
+    }
+
+    public String toString() {
+		String menu = "";
+		menu += "========[" + teamName + "]======== \n";
+		menu += "Coach " + userName + "'s Dashboard \n";
+		menu += "Week " + week + " \n";
+		menu += "~~~~~~~~~~~~~~~~~~~~\n";
+		menu += "[1] Team stats \n";
+		menu += "[2] Team budget \n";
+		menu += "[3] Equipment \n";
+		menu += "[4] Players \n";
+		menu += "[5] Game On! \n";
+		menu += "==================== \n";
+		return menu;
+    }
+
+    public void dashboard() {
+	String call = Keyboard.readString();
+	if( call.equals("1") ) {
+	    dashboard1();
+	    return;
+	}
+	if( call.equals("2") ) {
+	    dashboard2();
+	    return;
+	}
+	if( call.equals("3") ) {
+	    dashboard3();
+	    return;
+	}
+	if( call.equals("4") ) {
+	    dashboard4();
+	    return;
+	}
+	if( call.equals("5") ) {
+	    dashboard5();
+	    return;
+	}
+	else {
+	    System.out.println("\nInvalid choice\n");
+	    this.toString();
+	}
+	
+    }
+
+    public void dashboard1() { //List Players, View Stats
+	
+    }
+    
+    public void dashboard2() { //View Budget, View Payroll
+
+    }
+    
+    public void dashboard3() { //Buy Gear, Equip Gear
+	
+    }
+    
+    public void dashboard4() { //Draft Players, Team Roster
+	
+    }
+    
+    public void dashboard5() { //Play Game!!
+	playGame();
+	    
+    }
+
+    public void playGame() {
+	week++;
+	
+    }
+    
+    public static void main( String[] args ) {
+	Football Coach = new Football();
+	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	Coach.setupUser();
+	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	Coach.setupTeam();
+	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	System.out.println(Coach); //Week 1
+	Coach.dashboard(); 
+    }
+    
+}
+/* woo.java
+*   - main method <> dialogue
+*	<> make 5 teams
+*	
---
+++ v3/Players.java
@@ -0,0 +1,150 @@
+/*
+public class Players {
+
+    //~~~~~~~~INSTANCE VARIABLES~~~~~~~~
+    public int playerRating;   //the overall rating of the player
+    public static Quarterback myQB;
+    public Receiver myWR1;
+    public Receiver myWR2;
+    public Receiver myWR3;
+    public Runningback myRB;
+    public Linebacker myLB1;
+    public Linebacker myLB2;
+    public Cornerback myCB1;
+    public Cornerback myCB2;
+    public Safety mySS;
+    //static Quarterback EliManning = new Quarterback("Eli Manning", "Giants");
+    
+    static Quarterback EliManning = new Quarterback("Eli Manning", "Giants");
+
+    public static void setPlayers() {
+    	EliManning.setStats( 59 , 68 , 72 , 82 , 69 );
+    	EliManning.setPlayerRating();
+    }
+
+    //~~~~~~~~CONSTRUCTOR~~~~~~~~
+    public Players() {
+    }
+    
+    //~~~~~~~~METHODS~~~~~~~~
+    public void setPlayerRating( int rating ) {
+	playerRating = rating;
+    }
+
+    public void showStats() {
+
+    }
+
+    public static void setDefaultTeam( String team ) {
+	if( team.equals("Giants") ) {
+		//Quarterback EliManning = new Quarterback("Eli Manning", "Giants");
+	    //EliManning.setStats( 59 , 68 , 72 , 82 , 69 );
+	    setStats();
+	    setQB( EliManning );
+	    /*setWR1( OdellBeckham );
+	    setWR2( SterlingShepard );
+	    setWR3( BrandonMarshall );
+	    setRB( OrleansDarkwa );
+	    setLB1( JonathanCasillas );
+	    setLB2( CurtisGrant );
+	    setCB1( EliApple );
+	    setCB2( DarrylMorris );
+	    setSS( CooperTaylor );*/
+	}
+	/*if( team.equals("Eagles") ) {
+        Quarterback CarsonWentz = new Quarterback("Carson Wentz", "Eagles");
+        CarsonWentz.setStats( 63 , 66 , 74 , 79 , 68 );
+	    setQB( CarsonWentz );
+	    setWR1( AlshonJeffery );
+	    setWR2( NelsoNAgolor );
+	    setWR3( MackHollins );
+	    setRB( JayAjayi );
+	    setLB1( JoeWalker );
+	    setLB2( MychalKendricks );
+	    setCB1( PatrickRobinson );
+	    setCB2( NolanCarroll );
+	    setSS( MalcolmJenkins );
+	}
+	if( team.equals("Browns") ) {
+	    setQB( CodyKessler );
+	    setWR1( JoshGordon );
+	    setWR2( CoreyColeman );
+	    setWR3( KasenWilliams );
+	    setRB( IsaiahCrowell );
+	    setLB1( JamieCollins );
+	    setLB2( JoeSchobert );
+	    setCB1( JoeHaden );
+	    setCB2( JasonMccourty );
+	    setSS( DonteWhitner );
+	}
+	if( team.equals("Steelers") ) {
+	    setQB( BenRoethlisberger );
+	    setWR1( AntonioBrown );
+	    setWR2( JujuSmith );
+	    setWR3( EliRogers );
+	    setRB( LeveonBell );
+	    setLB1( RyanShazier );
+	    setLB2( TJWatt );
+	    setCB1( WilliamGay );
+	    setCB2( IkeTaylor );
+	    setSS( MikeMitchell );
+	}
+	if( team.equals("Falcons") ) {
+	    setQB( MattRyan );
+	    setWR1( JulioJones );
+	    setWR2( MohamedSanu );
+	    setWR3(TaylorGabriel );
+	    setRB( DevontaFreeman );
+	    setLB1( DieonJones );
+	    setLB2( DukeRiley );
+	    setCB1( DesmondTrufant );
+	    setCB2( RobertMcclain );
+	    setSS( WilliamMoore );*/
+	else {
+
+	}
+	
+    }
+    
+    public static void setQB( Quarterback qb ) {
+	myQB = qb;
+	System.out.print("My Quarterback is " + myQB);
+    }
+    /*public static void readQB( Quarterback qb ) {
+    	System.out.print(qbname);
+    }*/
+    public void setWR1( Receiver wr1 ) {
+	myWR1 = wr1;
+    }
+    public void setWR2( Receiver wr2 ) {
+	myWR2 = wr2;
+    }
+    public void setWR3( Receiver wr3 ) {
+	myWR3 = wr3;
+    }
+    public void setRB( Runningback rb ) {
+	myRB = rb;
+    }
+    public void setLB1( Linebacker lb1 ) {
+	myLB1 = lb1;
+    }
+    public void setLB2( Linebacker lb2 ) {
+	myLB2 = lb2;
+    }
+    public void setCB1( Cornerback cb1 ) {
+	myCB1 = cb1;
+    }
+    public void setCB2( Cornerback cb2 ) {
+	myCB2 = cb2;
+    }
+    public void setSS( Safety ss ) {
+	mySS = ss;
+    }
+    
+    
+    public static void main( String[] args ) {
+    	setDefaultTeam("Giants");
+    }
+    
+}
+*/
